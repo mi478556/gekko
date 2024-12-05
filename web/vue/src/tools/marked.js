49ad4125a415ce53e12c183bcbd21517bcea2b85
mi478556 <brown169@knights.ucf.edu>
2024-12-05 17:16:23 -0500

    update marked

--- web/vue/src/tools/marked.js
+++ web/vue/src/tools/marked.js
@@ -1,24 +1,25 @@
-const marked = require('marked');
+import { marked } from 'marked';
 
-// add `target='_blank'` to outgoing links
+// Customize the link rendering to open in a new tab
+const renderer = new marked.Renderer();
 
-// https://github.com/chjj/marked/pull/451#issuecomment-49976076
-
-var myRenderer = new marked.Renderer();
-myRenderer.link = function(href, title, text) {
-  var external, newWindow, out;
-  external = /^https?:\/\/.+$/.test(href);
-  newWindow = external || title === 'newWindow';
-  out = "<a href=\"" + href + "\"";
+renderer.link = function (href, title, text) {
+  const external = /^https?:\/\/.+$/.test(href);
+  const newWindow = external || title === 'newWindow';
+  let out = `<a href="${href}"`;
   if (newWindow) {
     out += ' target="_blank"';
   }
   if (title && title !== 'newWindow') {
-    out += " title=\"" + title + "\"";
+    out += ` title="${title}"`;
   }
-  return out += ">" + text + "</a>";
+  out += `>${text}</a>`;
+  return out;
 };
 
-marked.setOptions({renderer: myRenderer});
+// Apply the custom renderer
+marked.setOptions({
+  renderer: renderer,
+});
 
 export default marked;
